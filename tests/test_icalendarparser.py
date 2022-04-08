e2b5a438f497402f46878f82e6713b9cd58347b1
Thomas Francis Jr <franc@tcsaf.com>
2022-04-07 21:47:48 -0400

    Moved assert into own test and made better; added more docstrings

--- tests/test_icalendarparser.py
+++ tests/test_icalendarparser.py
@@ -3,21 +3,25 @@ from custom_components.ics_calendar.icalendarparser import ICalendarParser
 
 
 class TestICalendarParser:
+    """Test ICalendarParser class."""
+
     def test_get_class_finds_rie(self):
+        """Test get_class returns the rie parser."""
         assert ICalendarParser.get_class("rie") is not None
 
     def test_get_class_finds_ics(self):
+        """Test get_class returns the ics parser."""
         assert ICalendarParser.get_class("ics") is not None
 
     def test_get_class_does_not_find_unknown(self):
+        """Test get_class does not find non-existing parser."""
         assert ICalendarParser.get_class("unknown") is None
 
     def test_get_instance_returns_ICalendarParser(self):
+        """Test that get_instance returns instances of ICalendarParser."""
         assert isinstance(ICalendarParser.get_instance("rie"), ICalendarParser)
         assert isinstance(ICalendarParser.get_instance("ics"), ICalendarParser)
-        assert (
-            isinstance(
-                ICalendarParser.get_instance("unknown"), ICalendarParser
-            )
-            is False
-        )
+
+    def test_get_instance_returns_None(self):
+        """Test that get_instance returns None for non-existing parser."""
+        assert ICalendarParser.get_instance("unknown") is None
