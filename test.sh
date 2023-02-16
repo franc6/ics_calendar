b856d0d9624bc90f4bab1bf6f2830474106854c9
Thomas Francis Jr <franc@tcsaf.com>
2023-02-16 17:53:44 -0500

    Update scripts since pyproject.toml exists

--- test.sh
+++ test.sh
@@ -6,5 +6,5 @@ fi
 
 TOP=`pwd`
 rm -rf htmlcov/*
-pytest --allow-unix-socket --cov=custom_components.ics_calendar --cov-branch --cov-report html ${TEST}
+pytest ${TEST}
 echo "To view coverage, open ${TOP}/htmlcov/index.html"
