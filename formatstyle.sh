eb035a2b568cb2a3ec492a783c0b9bf4133d8c5e
Thomas Francis Jr <franc@tcsaf.com>
2024-09-10 18:03:57 -0400

    Update formatstyle.sh to format and validate JSON files

--- formatstyle.sh
+++ formatstyle.sh
@@ -5,6 +5,19 @@ isort custom_components/ics_calendar tests
 echo "Formatting files"
 black custom_components/ics_calendar tests
 
+echo "Formatting json files"
+for i in custom_components/ics_calendar/*.json custom_components/ics_calendar/translations/*.json
+do
+    echo "    $i"
+    python -m json.tool $i > /dev/null || exit
+    python -m json.tool $i > $i.new
+    if diff $i $i.new >/dev/null 2>/dev/null
+    then
+        cat $i.new > $i
+    fi
+    rm $i.new
+done
+
 echo "flake8 style and complexity checks"
 flake8 || exit
 
