1530bfa80685fac13a63e03ed01facc0cb00bfd3
Thomas Francis Jr <franc@tcsaf.com>
2025-01-20 10:49:39 -0500

    Allow pre-release dependencies for unit tests

--- test.sh
+++ test.sh
@@ -6,5 +6,5 @@ fi
 
 TOP=`pwd`
 rm -rf htmlcov/* coverage.xml
-PYTHONDONTWRITEBYTECODE=1 uv run pytest ${TEST}
+PYTHONDONTWRITEBYTECODE=1 uv run --prerelease=allow pytest ${TEST}
 echo "To view coverage, open ${TOP}/htmlcov/index.html"
